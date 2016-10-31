206eabd1d8b66df36279fb7d1f60902190fe5865
Alzater <alzater@mail.ru>
2016-11-01 00:49:53 +0300

    vector: push_back and emplace_back

--- vector.cpp
+++ vector.cpp
@@ -20,12 +20,19 @@ public:
     T* end();
 
 public:
+    void push_back( const T& elem );
+    T& emplace_back();
+
+public:
     void reserve( unsigned int size );
     void resize( unsigned int size );
 
 public:
     unsigned int size();
     bool empty();
+    
+private:
+    void grow();
 };
 
 template <typename T>
@@ -56,6 +63,24 @@ template <typename T>
 T* vector<T>::end()
 {
     return m_data + m_size;
+}
+
+template <typename T>
+void vector<T>::push_back( const T& elem )
+{
+    T& newElem = emplace_back();
+    newElem = elem;
+}
+
+template <typename T>
+T& vector<T>::emplace_back()
+{
+    if ( m_size == m_maxSize )
+        grow();
+
+    new (m_data + m_size) T();
+    
+    return *(m_data + m_size++);
 }
 
 template <typename T>
@@ -95,6 +120,22 @@ bool vector<T>::empty()
     return m_size == 0;
 }
 
+template <typename T>
+void vector<T>::grow()
+{
+    T* newData = static_cast<T*>( operator new[] (m_size * 2 * sizeof(T)) );
+    m_maxSize = m_size * 2;
+    
+    for ( unsigned int i = 0; i < m_size; ++i )
+    {
+        new (newData + i) T( m_data[i] );
+        m_data[i].~T();
+    }
+    
+    delete[] m_data;
+    m_data = newData;
+}
+
 }  //  edu
 
 int main()
@@ -105,9 +146,16 @@ int main()
     for ( int i = 0; i < 5; ++i )
         v[i] = 5 - i;
     
+        
+    v.push_back(10);
+    int& newElem = v.emplace_back();
+    newElem = 0;
+    
     for ( int* iter = v.begin(); iter < v.end(); ++iter )
     {
         std::cout << *iter << ' ';
     }
-    std::cout << v.size();
+
+    
+    std::cout << '\n' << v.size();
 }
